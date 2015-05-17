5e265c4e9c7b237716fa1ccdf3894fd1ae27f068
Rok Tavčar <roktav@gmail.com>
2015-05-17 17:49:23 +0200

    more settings
    added more settings options, also activity for adding/editing rooms

--- app/src/main/java/com/r00li/rhremote/NewRoom.java
+++ app/src/main/java/com/r00li/rhremote/NewRoom.java
@@ -1,38 +1,65 @@
 package com.r00li.rhremote;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class NewRoom extends ActionBarActivity {
+    String[] strings = {"Soba", "Kopalnica", "Kuhinja"};
+    int arr_images[] = {R.drawable.ic_bed, R.drawable.ic_shower, R.drawable.ic_bulb_vec};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String[] strings = {"Soba", "Kopalnica", "Kuhinja"};
+        int arr_images[] = {R.drawable.ic_bed, R.drawable.ic_shower, R.drawable.ic_bulb_vec};
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_room);
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        mySpinner.setAdapter(new MyAdapter(NewRoom.this, R.layout.row, strings));
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_room, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public class MyAdapter extends ArrayAdapter<String> {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public MyAdapter(Context context, int textViewResourceId, String[] objects) {
+            super(context, textViewResourceId, objects);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        public View getCustomView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View row = inflater.inflate(R.layout.row, parent, false);
+
+            TextView label = (TextView) row.findViewById(R.id.ImgName);
+            label.setText(strings[position]);
+
+
+            ImageView icon = (ImageView) row.findViewById(R.id.image);
+            icon.setImageResource(arr_images[position]);
+
+            return row;
+        }
     }
 }
--- app/src/main/java/com/r00li/rhremote/SettingsActivity.java
+++ app/src/main/java/com/r00li/rhremote/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.r00li.rhremote;
 
+import android.content.Intent;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,10 +9,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity  {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        setContentView(R.layout.activity_settings);
     }
+
+
 }
